a0ec5634b2a343d6ca10938ba0cb345e89d0b978
William Alejandro C.m <crazzy.wili1990@hotmail.com>
2025-03-15 21:45:57 -0500

    creacion de los respectivos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { VentasModule } from './ventas/ventas.module';
+import { EmpleadosModule } from './empleados/empleados.module';
+import { ProductosModule } from './productos/productos.module';
+import { RolesModule } from './roles/roles.module';
+import { InsumosModule } from './insumos/insumos.module';
 
 @Module({
-  imports: [],
+  imports: [VentasModule, EmpleadosModule, ProductosModule, RolesModule, InsumosModule],
   controllers: [AppController],
   providers: [AppService],
 })
